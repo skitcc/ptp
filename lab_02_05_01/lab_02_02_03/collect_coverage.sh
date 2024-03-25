e6c3a1c716d98f672f585e4f4348b7bc768f6648
Ivan <fmy.zikzak@yandex.ru>
2024-03-25 17:07:54 +0300

    life happens

--- lab_02_05_01/lab_02_02_03/collect_coverage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$SCRIPT_DIR" || exit
-gcc main.c --coverage -o app.exe -lm
-./func_tests/scripts/func_tests.sh
-gcov main.c
--- lab_02_05_01/lab_02_03_03/build_release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$SCRIPT_DIR" || exit
-gcc -std=c99 -Werror -Wall -Wpedantic -Wextra -Wfloat-conversion -Wfloat-equal -c main.c
-gcc main.o -o app.exe -lm
