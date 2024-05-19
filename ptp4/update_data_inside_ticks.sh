ac7d1fec546e259580cf434721cef052ccad10a5
lifeline <fmy.zikzak@yandex.ru>
2024-05-19 22:47:17 +0300

    done

--- ptp4/update_data_inside_ticks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for app in apps/apps_inside_ticks/*.exe; do
-    size=$(echo "$app" | cut -d'=' -f2 | cut -d'_' -f1)
-    method=$(echo "$app" | cut -d'=' -f3 | cut -d'.' -f1)
-    timestamp=$(date +%s)
-
-    $app > ./data/inside_ticks_data/raw_data/size={$size}_method={$method}_{$timestamp}.txt
-done
